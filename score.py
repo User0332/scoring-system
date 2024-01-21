04be4af5b0151dd999c2b3ba1346ef22860f0f69
User0332 <carlzfurtado@gmail.com>
2024-01-21 11:48:12 -0500

    add reset buttons

--- score.py
+++ score.py
@@ -7,6 +7,8 @@ SCREEN_H, SCREEN_W = 680, 1100
 
 plus_icon = pygame.transform.scale(pygame.image.load("assets/plus-icon.png"), (50, 50))
 del_icon = pygame.transform.scale(pygame.image.load("assets/del-icon.png"), (50, 50))
+reset_icon = pygame.transform.scale(pygame.image.load("assets/reset.png"), (50, 50))
+
 arial = pygame.font.SysFont("Arial", 30, True)
 
 win_title = input("Window Title: ")
@@ -26,6 +28,7 @@ team_starts = [i*team_height for i in range(numteams)]
 team_color_surfs = [pygame.Surface((SCREEN_W, team_height)) for _ in range(numteams)]
 team_add_rects = [pygame.Rect(((100+teamtext[i].get_width(), team_starts[i]+10)), (plus_icon.get_width(), plus_icon.get_height())) for i in range(numteams)]
 team_del_rects = [pygame.Rect(((170+teamtext[i].get_width(), team_starts[i]+10)), (plus_icon.get_width(), plus_icon.get_height())) for i in range(numteams)]
+team_reset_rects = [pygame.Rect(((240+teamtext[i].get_width(), team_starts[i]+10)), (reset_icon.get_width(), reset_icon.get_height())) for i in range(numteams)]
 
 for i, surf in enumerate(team_color_surfs):
 	surf.fill(team_colors[i])
@@ -53,6 +56,11 @@ while 1:
 					if rect.collidepoint(pos):
 						team_scores[i]-=add
 						break
+				else:
+					for i, rect in enumerate(team_reset_rects):
+						if rect.collidepoint(pos):
+							team_scores[i] = 0
+							break
 
 	for i, surf in enumerate(team_color_surfs):
 		screen.blit(surf, (0, team_starts[i]))
@@ -60,5 +68,6 @@ while 1:
 		screen.blit(arial.render(str(team_scores[i]), True, "black"), (teamtext[i].get_width()+30, team_starts[i]+10))
 		screen.blit(plus_icon, team_add_rects[i])
 		screen.blit(del_icon, team_del_rects[i])
+		screen.blit(reset_icon, team_reset_rects[i])
 
 	pygame.display.update()
